e385dadcb0f16cd591ddefc31b3c508f3c5c6d3c
KJBS2 <qja0950@gmail.com>
2016-12-05 22:43:38 +0900

    Commit in Window 10

--- mysite/settings.py
+++ mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_86_s)m&z20#n(hfkm#tr-c=uojk(6s*ov6o%h9b=5-d_f&crt'
+SECRET_KEY = 'o$xnkr&hdz^0h-sw+qhiorcf71ceqj6bu2l4bsm=2wq-715l@3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -101,4 +101,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
